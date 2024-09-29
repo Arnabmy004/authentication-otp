31f6d52740037add8ebf60c15cde8e8657bdce2f
Arnab <arnab1090.be22@chitkarauniversity.edu.in>
2024-09-29 18:30:34 +0530

    push

--- regapp/src/App.js
+++ regapp/src/App.js
@@ -1,28 +1,132 @@
-import React from 'react';
-import { FaLock } from 'react-icons/fa';
-import OTPInput, { ResendOTP } from "otp-input-react";
-import { useState } from 'react';
 
-const App = () => {
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { FaLock } from 'react-icons/fa';
+import {BsTelephoneFill} from 'react-icons/bs';
+import { CgSpinner } from "react-icons/cg";
+import OTPInput, { ResendOTP } from "otp-input-react";
+import PhoneInput from 'react-phone-input-2';
+import 'react-phone-input-2/lib/style.css';
+import { auth, RecaptchaVerifier, signInWithPhoneNumber } from "./firbase.config";
+import toast, { Toaster } from 'react-hot-toast';
+
+const App = () => { 
   const [otp, setOtp] = useState("");
+  const [ph, setPh] = useState("");
+  const [loading,setLoading]=useState(false);
+  const[showOTP,setShowOTP]=useState(false);
+  const [user,setUser]=useState(null);
+  const [confirmationResult, setConfirmationResult] = useState(null);
+
+  function onCapchaVerify(){
+    if(!window.recaptchaVerifier)
+    {
+      window.recaptchaVerifier = new RecaptchaVerifier(
+        'recaptcha-container', {
+
+        'size': 'invisible',
+        'callback': (response) => {
+           onSignup()
+        },
+          "expired-callback": () => {
+            toast.error('Recaptcha expired. Please try again.');
+          },
+        },
+        
+       auth 
+      );
+      window.recaptchaVerifier.render().catch((error) => {
+        console.error("Recaptcha rendering failed:", error);
+        toast.error('recapcha expired...please try again later');
+      });
+    }
+  }
+
+
+  function onSignup(){
+    setLoading(true);
+    onCapchaVerify();
+    const appVerifier=window.recaptchaVerifier
+    const formatph='+91'+ph;
+
+    signInWithPhoneNumber(auth, formatph, appVerifier)
+    .then((confirmationResult) => {
+     
+      window.confirmationResult = confirmationResult;
+      setLoading(false);
+      setShowOTP(true);
+      toast.success('OTP sent successfully..');
+    }).catch((error) => {
+       console.log(error);
+       setLoading(false);
+       toast.error('Failed to send OTP'); 
+    });
+  };
+
+  
+
   return (
     <section className="bg-emerald-500 flex items-center justify-center h-screen">
-    <div>
-      <div className='w-full flex-col gap-4 rounded-lg p-4'>
-            <h1 className='text-center leading normal text-white font medium text-3xl mb-6'>Welcome to <br/>CODE A PROGRAMME</h1>
-            <>
+      <div>
+        <Toaster toastOptions={{duration:4000}}/>
+        <div id="recaptcha-container"></div>
+        { user ?(
+        <h2 className='text-center text-white font medium text-3xl mb-6'>
+        Login Success
+    </h2>
+         ):(
+        <div className='w-full flex-col gap-4 rounded-lg p-4'>
+          <h1 className='text-center leading normal text-white font medium text-3xl mb-6'>
+            Welcome to <br />CODE A PROGRAMME</h1>
+
+          {
+                showOTP ?(
+
+          <>
             <div className='bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full'>
-               <FaLock size={30}/>
+              <FaLock size={30} />
             </div>
-            <label htmlFor='ph' 
-            className='font-bold text-2xl text-white flex justify-center'
+            <label htmlFor='otp'
+              className='font-bold text-xl text-white flex justify-center mb-4'
             >
               Enter your OTP
             </label>
-            <OTPInput value={otp} onChange={setOtp} OTPLength={6} otpType='number' disabled={false} autoFocus className="otp-container" secure></OTPInput>
-              </>
+            <OTPInput value={otp} onChange={setOtp} OTPLength={6} otpType='number' disabled={false} autoFocus className="otp-container"></OTPInput>
+            <button className='bg-emerald-600 w-full flex gap-1 items-center justify-center py-2 text-white rounded'>
+              {
+                loading &&  <CgSpinner size={20}  className='mt-1 animate-spin'/>
+              }
+             
+              <span>Verify OTP</span>
+
+            </button>
+          </> ):(
+          <>
+            <div className='bg-white text-emerald-500 w-fit mx-auto p-4 rounded-full'>
+              <BsTelephoneFill size={30} />
+            </div>
+            <label htmlFor=''
+              className='font-bold text-xl text-white flex justify-center mb-2'
+            >
+              Verify your phone number
+            </label>
+            <PhoneInput  country={"in"} value={ph} onChange={setPh} className='mb-4'/>
+            <button onClick={onSignup} 
+            className='bg-emerald-600 w-full flex gap-1 items-center justify-center py-2 text-white rounded'>
+              {
+                loading &&  <CgSpinner size={20}  className='mt-1 animate-spin'/>
+              }
+             
+              <span>Send code via SMS</span>
+
+            </button>
+          </>
+         )}
+    
+        </div>
+        )} 
+       
       </div>
-    </div>
     </section>
   );
 };
--- regapp/src/firbase.config.js
+++ regapp/src/firbase.config.js
@@ -2,11 +2,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyC4yOorRggow0OWXRCTGHPWK63L2cu-Wqo",
   authDomain: "otp-project-aa69c.firebaseapp.com",
@@ -19,3 +21,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth=getAuth(app);
+auth.settings.appVerificationDisabledForTesting = true;
+ export { auth, RecaptchaVerifier, signInWithPhoneNumber };
+
